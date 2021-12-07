51e803ca04f8f2ede80829516f0f2176d960675f
mohammed eldeeb <01158765343>
2021-12-07 13:27:33 +0200

    eldeeb

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const yargs= require("yargs");
+
+const fs= require("fs");
+
+const date=require("myModile") ;
+
+
+// yargs.command({
+//     command:"add",
+//     description:"add some logic",
+//     builder:{
+//         titel:{
+//             describe: 'a unique identifier for the server',
+//             // demandOption:true,
+//             type: 'string'
+//         },
+//         name:{
+//             describe:'halow from ather side',
+//             demandOption:true,
+//             type: 'string'
+//         },
+        
+//     },
+//     handler:()=>{
+//         console.log("good")
+        
+//         console.log(yargs.argv.titel,"add");
+
+//     }
+// })
+// console.log(yargs.argv.titel,"add");
+
+// console.log(yargs.argv);
+
+// let a=JSON.parse(date.porson.toString())
+fs.writeFileSync('notes.json',date.porson.toString())
+
+const myFile=fs.readFileSync("./notes.txt").toString()
+console.log(myFile)
---
+++ myModile.js
@@ -0,0 +1,11 @@
+
+const porson =[
+    {name :"mohameed" ,age:43 ,jop :"frontEnd developer"},
+    {name :"raneem" ,age:23 ,jop :"narnStact developer"}
+]
+const cars=[ "BMW" ,"Marcids","jeep"];
+
+module.exports={
+    porson,
+    cars,
+}
